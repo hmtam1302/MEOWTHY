31c7a8b2fd4fc0e7a19e8ae5609871aa6772199b
TranDinhLe <75212466+TranDinhLe@users.noreply.github.com>
2022-05-30 01:08:48 +0700

    Update Food.js

--- server/routes/Food.js
+++ server/routes/Food.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Food = require("../models/Food");
+const FedFood = require("../models/FedFood");
 
 require("dotenv").config();
 
@@ -27,18 +28,177 @@ require("dotenv").config();
  *          type: number
  *          description: Calories per amount kcal
  *      example:
- *        name: "cơm",
- *        amount: 10,
- *        calories: 13
+ *        {
+ *          name: "cơm",
+ *          amount: 10,
+ *          calories: 13
+ *        }
  */
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    ListFood:
+ *      type: array
+ *      items:
+ *         $ref: '#/components/schemas/Food'
+ */
+
 //------------------------------------------------------
 
-
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    summary: Get all food available
+ *    tags: [Food]
+ *    responses:
+ *      '200':
+ *        description: Successful response
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                data:
+ *                  $ref: '#/components/schemas/ListFood'
+ *      '500':
+ *        description: Error response
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  description: Error message
+ *
+ */
 router.get("/food", async (req, res) => {
   try {
       const listFood = await Food.find({});
       return res.status(200).json({ data: listFood });
   } catch (err) {
     res.status(500).json({ message: JSON.stringify(err) });
+  }
+});
+
+
+
+/**
+ * @swagger
+ * /{foodId}:
+ *  put:
+ *    summary: Change food in record
+ *    tags: [FedFood]
+ *    parameters:
+ *      - in: path
+ *        name: foodId
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: food Id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json: 
+ *          schema:
+ *            $ref: '#/components/schemas/FedFoodUpdate'
+ *    responses:
+ *      '200':
+ *        description: Successful response
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  description: Success message
+ *      '500':
+ *        description: Error response
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  description: Error message
+ * 
+ *  delete:
+ *    summary: Delete a food in record
+ *    tags: [FedFood]
+ *    parameters:
+ *      - in: path
+ *        name: foodId
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: food Id
+ *    responses:
+ *      '200':
+ *        description: Successful response
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  description: Success message
+ *      '500':
+ *        description: Error response
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  description: Error message
+ *
+ */
+
+ router.put("/food/:foodId", async (req, res) => {
+  const { foodId } = req.params
+  const { amount, calories } = req.body;
+  const fed_food = await FedFood.find({ _id: foodId });
+  const diary = await Diary.find({ diaryId: fed_food._doc.diaryId });
+  if (diary.length === 0) {
+    res.status(500).json({ message: "Diary at the date not found" });
+  } else {
+      diary._doc.food_calories += calories - fed_food._doc.calories;
+      FedFood.findOneAndUpdate({ _id : foodId }, {amount : amount, calories : calories})
+      .then(() => {
+        res.status(200).json({ message: "Update successful!" })
+      })
+      .catch((err) =>
+        res
+          .status(500)
+          .json({ message: JSON.stringify(err) })
+      );
+  }
+});
+
+
+router.delete("/food/:foodId", async (req, res) => {
+  const { foodId } = req.params;
+  const response = await FedFood.find({ _id: foodId });
+  const diary = await Diary.find({ diaryId: response._doc.diaryId });
+  if (response.length === 0) {
+    res.status(500).json({ message: "Selected food not found" });
+  } else {
+    diary._doc.calories -= response._doc.calories;
+    FedFood.findOneAndDelete({ _id : foodId })
+    .then(() => {
+      res.status(200).json({ message: "Delete successful!" })
+    })
+    .catch((err) =>
+      res
+        .status(500)
+        .json({ message: JSON.stringify(err) })
+    );
   }
 });
