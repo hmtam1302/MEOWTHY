c14dadd9afe7f841e0712b1b2201a619ebd80c91
hoatruonghong <70649552+hoatruonghong@users.noreply.github.com>
2022-06-01 22:10:25 +0700

    update login

--- mobile/navigation/LoginSign.js
+++ mobile/navigation/LoginSign.js
@@ -4,6 +4,7 @@ import {
   } from "@react-navigation/bottom-tabs";
   import { createNativeStackNavigator } from "@react-navigation/native-stack";
   import { NavigationContainer } from "@react-navigation/native";
+  import React, {useContext} from 'react';
 
   import Login from "./screen/Login/Login";
   import Signup from "./screen/Signup/Signup";
@@ -56,173 +57,25 @@ import {
   const nameLoginChange = "LoginChange";
 
   const Stack = createNativeStackNavigator();
-  const Tab = createBottomTabNavigator();
-
-  function DiaryStack() {
-    return (
-      <Stack.Navigator initialRouteName={nameDiary}>
-        <Stack.Screen
-          name={nameDiary}
-          component={Diary}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name={nameFood}
-          component={Food}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name={nameAboutCat}
-          component={AboutCat}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name={nameExercise}
-          component={Exercise}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    );
-  }
-  
-  function HomeStack() {
-    return (
-      <Stack.Navigator initialRouteName={nameHome}>
-        <Stack.Screen
-          name={nameHome}
-          component={Home}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name={nameListCat}
-          component={ListCat}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name={nameAddCat}
-          component={AddCat}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name={nameUpdateCat}
-          component={UpdateCat}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    );
-  }
-
   function LoginSign() {
-    const {userInfo, onboardLoading} = useContext(AuthContext);
+    const {isLoggedIn} = React.useContext(AuthContext);
+    //const  isLoggedIn = false;
     return (
     <NavigationContainer>
       <Stack.Navigator>
-      {onboardLoading ? (
-        <Stack.Screen
-        name={nameOnboarding}
-        component={Onboarding}
-        options={{ headerShown: false }}
-        />
-      ) : userInfo.access_token ? (
-        <Tab.Navigator
-          initialRouteName={nameHome}
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-              let rn = route.name;
-  
-              if (rn === nameDiary) {
-                iconName = focused ? "calendar" : "calendar-outline";
-              } else if (rn === nameNotification) {
-                iconName = focused ? "notifications" : "notifications-outline";
-              } else if (rn === nameUser) {
-                iconName = focused ? "person" : "person-outline";
-              } else if (rn === nameHome) {
-                iconName = focused ? "home" : "home-outline";
-              } else if (rn === nameReport) {
-                iconName = focused ? "bar-chart" : "bar-chart-outline";
-              }
-  
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: colors.yellow,
-            tabBarInactiveTintColor: colors.gray,
-          })}
-          tabBarOptions={{
-            lableStyle: { paddingBottom: 10, fontSize: 10, fontWeight: "bold" },
-            style: { padding: 10, height: 70 },
-          }}
-        >
-          <Tab.Screen
-            options={{ headerShown: false }}
-            name={nameHome}
-            component={HomeStack}
-          />
-          <Tab.Screen
-            options={{ headerShown: false }}
-            name={nameDiary}
-            component={DiaryStack}
-          />
-          <Tab.Screen
-            options={{ headerShown: false }}
-            name={nameReport}
-            component={Report}
-          />
-          <Tab.Screen
-            options={{ headerShown: false }}
-            name={nameNotification}
-            component={Notification}
-          />
-          <Tab.Screen
-            options={{ headerShown: false }}
-            name={nameUser}
-            component={User}
-          />
-        </Tab.Navigator>
+      {isLoggedIn ? (
+        <>
+          <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
+        </>
       ) : (
         <>
-        <Stack.Screen
-          name={nameLogin}
-          component={Login}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name={nameSignup}
-          component={Signup}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name={nameSignup2}
-          component={Signup2}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name={nameSignupGoal}
-          component={SignupGoal}
-          options={{ headerShown: false }}
-        />
-        
-        <Stack.Screen
-          name={nameLogin1}
-          component={LoginForgot1}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name={nameLogin2}
-          component={LoginForgot2}
-          options={{ headerShown: false }}
-        />
-        
-        <Stack.Screen
-          name={nameLoginChange}
-          component={LoginChange}
-          options={{ headerShown: false }}
-        />
+          <Stack.Screen name={nameLogin} component={Login} options={{headerShown: false}}/>
         </>
-        )}
+      )}
       </Stack.Navigator>
     </NavigationContainer>
+
     );
-  }
+  };
   
   export default LoginSign;
